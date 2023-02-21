113d4c05eb6e6c20fc0aae07d685a3e40683d40a
Dami <chltpgusg@naver.com>
2023-02-22 00:12:32 +0900

    Feat: add Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 
 import GlobalStyle from "@/styles/global-styles";
+import ChannelTalk from "@/components/ChannelTalk";
+import Nav from "@/components/Nav";
 const Providers = dynamic(() => import("@/components/Providers"), {
   ssr: false,
 });
@@ -10,7 +12,11 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Providers>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ChannelTalk />
+      <>
+        <Nav />
+        <Component {...pageProps} />
+      </>
     </Providers>
   );
 };
