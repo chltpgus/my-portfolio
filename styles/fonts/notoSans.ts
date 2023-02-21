d0108b084c45286fd806625af2427380703f5b6b
Dami <chltpgusg@naver.com>
2023-02-22 00:13:16 +0900

    Fix: 오타 수정

--- styles/fonts/notoSans.ts
+++ styles/fonts/notoSans.ts
@@ -1,34 +1,30 @@
-import { Noto_Sans_KR } from "@next/font/google";
+import { Noto_Sans_KR } from '@next/font/google';
 
 const bold = Noto_Sans_KR({
-  weight: "700",
-  variable: "--noto-sans_KR-bold",
-  display: "fallback",
-  subsets: ["latin"],
-  style: "normal",
-  fallback: ["system-ui"],
+    weight: '700',
+    variable: '--noto-sans_KR-bold',
+    display: 'fallback',
+    subsets: ['latin'],
+    style: 'normal',
+    fallback: ['system-ui'],
 });
 
 const regular = Noto_Sans_KR({
-  weight: "400",
-  display: "fallback",
-  subsets: ["latin"],
-  style: "normal",
-  variable: "--noto-sans_KR-regular",
-  fallback: ["system-ui"],
+    weight: '400',
+    display: 'fallback',
+    subsets: ['latin'],
+    style: 'normal',
+    variable: '--noto-sans_KR-regular',
+    fallback: ['system-ui'],
 });
 
 const medium = Noto_Sans_KR({
-  weight: "500",
-  display: "fallback",
-  subsets: ["latin"],
-  style: "normal",
-  variable: "--noto-sans_KR-medium",
-  fallback: ["system-ui"],
+    weight: '500',
+    display: 'fallback',
+    subsets: ['latin'],
+    style: 'normal',
+    variable: '--noto-sans_KR-medium',
+    fallback: ['system-ui'],
 });
 
-export {
-  bold as notoSansKrBold,
-  regular as notoSansKrRgular,
-  medium as notoSansKrMedium,
-};
+export { bold as notoSansKrBold, regular as notoSansKrRegular, medium as notoSansKrMedium };
