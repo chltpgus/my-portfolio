dbcaa11037e60df047147d29e51874b987ea995c
Dami <chltpgusg@naver.com>
2023-02-26 23:10:58 +0900

    Style: zIndex

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -6,12 +6,14 @@ interface ContainerProps {
   backgroundColor?: string;
 }
 
-const Container = styled.section<ContainerProps>`
+const Container = styled.div<ContainerProps>`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
   background-color: ${props => props?.backgroundColor ? props?.backgroundColor : ''};
   max-width : ${maxWidth}px;
+  z-index:1;
 `;
 
 export default Container;
